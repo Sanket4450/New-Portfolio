e2a247cffdba29fff905ec3e7769b622abcae4c8
Sanket4450 <talaviyasanket01@gmail.com>
2025-04-17 00:15:48 +0530

    main content added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
+import { Main } from './components/Main/Main'
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Footer/>
+      <Main />
+      <Footer />
     </>
   )
 }
